4f3457b731b1eddf4885db99689bb74456244c77
Jonas Otter <kriakiku@icloud.com>
2024-10-02 12:29:43 +0000

    feat: added phantombot

--- kriakiku-phantombot/entrypoint.sh
+++ kriakiku-phantombot/entrypoint.sh
@@ -3,34 +3,35 @@
 # Certificate parameters
 DOMAIN="umbrel.local"
 CERT_DIR="/umbrel_certs"
-CERT_FILE="$CERT_DIR/$DOMAIN.crt"
-KEY_FILE="$CERT_DIR/$DOMAIN.key"
+PEM_FILE="$CERT_DIR/$DOMAIN.pem"
 DAYS_VALID=825  # Maximum certificate validity period
 
-# Check if the certificate exists
-if [ ! -f "$CERT_FILE" ]; then
-  echo "Certificate not found, creating a new one..."
+# Check if the PEM certificate exists
+if [ ! -f "$PEM_FILE" ]; then
+  echo "PEM certificate not found, creating a new one..."
   mkdir -p "$CERT_DIR"
+  # Generate private key and certificate in PEM format
   openssl req -new -newkey rsa:2048 -days $DAYS_VALID -nodes -x509 \
-    -keyout "$KEY_FILE" -out "$CERT_FILE" \
+    -keyout "$PEM_FILE" -out "$PEM_FILE" \
     -subj "/CN=$DOMAIN"
-  echo "Certificate successfully created."
-else
-  # Check the certificate's expiration date
-  EXPIRY_DATE=$(openssl x509 -enddate -noout -in "$CERT_FILE" | cut -d= -f2)
-  EXPIRY_TIMESTAMP=$(date -d "$EXPIRY_DATE" +%s)
-  CURRENT_TIMESTAMP=$(date +%s)
-  THREE_MONTHS=$(($CURRENT_TIMESTAMP + 90*24*3600))
+  echo "PEM certificate successfully created."
+  exit 0
+fi
 
-  if [ "$EXPIRY_TIMESTAMP" -lt "$THREE_MONTHS" ]; then
-    echo "Certificate will expire in less than 3 months. Issuing a new certificate..."
-    openssl req -new -newkey rsa:2048 -days $DAYS_VALID -nodes -x509 \
-      -keyout "$KEY_FILE" -out "$CERT_FILE" \
-      -subj "/CN=$DOMAIN"
-    echo "Certificate successfully updated."
-  else
-    echo "Certificate is valid until $EXPIRY_DATE. No update required."
-  fi
+# Check the certificate's expiration date
+EXPIRY_DATE=$(openssl x509 -enddate -noout -in "$PEM_FILE" | cut -d= -f2)
+EXPIRY_TIMESTAMP=$(date -d "$EXPIRY_DATE" +%s)
+CURRENT_TIMESTAMP=$(date +%s)
+THREE_MONTHS=$(($CURRENT_TIMESTAMP + 90*24*3600))
+
+if [ "$EXPIRY_TIMESTAMP" -lt "$THREE_MONTHS" ]; then
+  echo "Certificate will expire in less than 3 months. Issuing a new PEM certificate..."
+  openssl req -new -newkey rsa:2048 -days $DAYS_VALID -nodes -x509 \
+    -keyout "$PEM_FILE" -out "$PEM_FILE" \
+    -subj "/CN=$DOMAIN"
+  echo "PEM certificate successfully updated."
+else
+  echo "PEM certificate is valid until $EXPIRY_DATE. No update required."
 fi
 
 # Run the PhantomBot entrypoint script
