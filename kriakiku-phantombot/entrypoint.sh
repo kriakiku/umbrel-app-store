0fa0e2e40c96318f8685cc7e1bec8655b804e00b
Jonas Otter <kriakiku@icloud.com>
2024-10-02 11:35:57 +0000

    feat: added phantombot

--- kriakiku-phantombot/entrypoint.sh
+++ kriakiku-phantombot/entrypoint.sh
@@ -15,21 +15,24 @@ if [ ! -f "$CERT_FILE" ]; then
     -keyout "$KEY_FILE" -out "$CERT_FILE" \
     -subj "/CN=$DOMAIN"
   echo "Certificate successfully created."
-  exit 0
-fi
-
-# Check the certificate's expiration date
-EXPIRY_DATE=$(openssl x509 -enddate -noout -in "$CERT_FILE" | cut -d= -f2)
-EXPIRY_TIMESTAMP=$(date -d "$EXPIRY_DATE" +%s)
-CURRENT_TIMESTAMP=$(date +%s)
-THREE_MONTHS=$(($CURRENT_TIMESTAMP + 90*24*3600))
-
-if [ "$EXPIRY_TIMESTAMP" -lt "$THREE_MONTHS" ]; then
-  echo "Certificate will expire in less than 3 months. Issuing a new certificate..."
-  openssl req -new -newkey rsa:2048 -days $DAYS_VALID -nodes -x509 \
-    -keyout "$KEY_FILE" -out "$CERT_FILE" \
-    -subj "/CN=$DOMAIN"
-  echo "Certificate successfully updated."
 else
-  echo "Certificate is valid until $EXPIRY_DATE. No update required."
+  # Check the certificate's expiration date
+  EXPIRY_DATE=$(openssl x509 -enddate -noout -in "$CERT_FILE" | cut -d= -f2)
+  EXPIRY_TIMESTAMP=$(date -d "$EXPIRY_DATE" +%s)
+  CURRENT_TIMESTAMP=$(date +%s)
+  THREE_MONTHS=$(($CURRENT_TIMESTAMP + 90*24*3600))
+
+  if [ "$EXPIRY_TIMESTAMP" -lt "$THREE_MONTHS" ]; then
+    echo "Certificate will expire in less than 3 months. Issuing a new certificate..."
+    openssl req -new -newkey rsa:2048 -days $DAYS_VALID -nodes -x509 \
+      -keyout "$KEY_FILE" -out "$CERT_FILE" \
+      -subj "/CN=$DOMAIN"
+    echo "Certificate successfully updated."
+  else
+    echo "Certificate is valid until $EXPIRY_DATE. No update required."
+  fi
 fi
+
+# Run the PhantomBot entrypoint script
+echo "Running PhantomBot entrypoint script..."
+/opt/PhantomBot/docker-entrypoint.sh
